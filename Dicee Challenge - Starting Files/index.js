36bc1b96879f8f2a290e85a8481e9baad46be932
kaushal <kaushaljainer@gmail.com>
2021-06-17 17:40:02 +0530

    feature-dice sound and button to play add

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,20 +1,54 @@
-// random number generation for both the dice.
+document.querySelector("button").addEventListener( "click" , function() {
 
-let randomNumber1 = Math.floor(Math.random()*6)+1;
-let randomNumber2 = Math.floor(Math.random()*6)+1;
 
-//changing the image attribute.
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+  //function for animation
 
-if(randomNumber1===randomNumber2) {
-  document.querySelector("h1").innerHTML=" Match Draw ";
+  buttonAnimation();
+
+  //for sound
+  diceRollSound();
+
+  //function to generate dice roll
+  diceRoll();
+
+
+} )
+
+//dice roll diceRollSound
+
+function diceRollSound(){
+  let rollSound=new Audio("sounds/ONEDICE.wav");
+  rollSound.play();
 }
 
-else if(randomNumber1>randomNumber2) {
-  document.querySelector("h1").innerHTML="🏆 Player1 Won";
+//button animation
+function buttonAnimation(){
+  document.querySelector("button").classList.add("pressed");
+  setTimeout(function(){
+  document.querySelector("button").classList.remove("pressed");
+},100)
+
 }
 
-else {
-  document.querySelector("h1").innerHTML="Player2 Won 🏆";
+// to generate dice roll
+function diceRoll(){
+  let randomNumber1 = Math.floor(Math.random()*6)+1;
+  let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+  //changing the image attribute.
+  document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+  document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+  if(randomNumber1===randomNumber2) {
+    document.querySelector("h1").innerHTML=" Match Draw ";
+  }
+
+  else if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="🏆 Player1 Won";
+  }
+
+  else {
+    document.querySelector("h1").innerHTML="Player2 Won 🏆";
+  }
+
 }
