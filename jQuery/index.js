cd5b88995eab5bda0dd1f713b3df424352f210b9
kaushal <kaushaljainer@gmail.com>
2021-06-19 13:22:01 +0530

    practice jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,8 @@
-$("h1").css("color","red");
+$("button").click(function(){
+  this.style.backgroundColor="red";
+  $("h1").css("font-size","10rem");
+})
+
+$("input").keydown(function(event){
+  console.log(event.key);
+});
